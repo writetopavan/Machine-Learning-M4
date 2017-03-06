9baf34777f27b9dbd9d984011478e3d9d4d5a924
Pavan Kumar Singh <writetopavan@gmail.com>
2017-03-06 10:29:41 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+import scipy.io
+import random, math
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from sklearn import manifold
+from sklearn.decomposition import PCA
+
+def Plot2D(T, title, x, y, num_to_plot=40):
+  # This method picks a bunch of random samples (images in your case)
+  # to plot onto the chart:
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.set_title(title)
+  ax.set_xlabel('Component: {0}'.format(x))
+  ax.set_ylabel('Component: {0}'.format(y))
+  x_size = (max(T[:,x]) - min(T[:,x])) * 0.08
+  y_size = (max(T[:,y]) - min(T[:,y])) * 0.08
+  for i in range(num_to_plot):
+    img_num = int(random.random() * num_images)
+    x0, y0 = T[img_num,x]-x_size/2., T[img_num,y]-y_size/2.
+    x1, y1 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2.
+    img = df.iloc[img_num,:].reshape(num_pixels, num_pixels)
+    ax.imshow(img, aspect='auto', cmap=plt.cm.gray, interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
+
+  # It also plots the full scatter:
+  ax.scatter(T[:,x],T[:,y], marker='.',alpha=0.7)
+
+
+
+# A .MAT file is a .MATLAB file. The faces dataset could have came
+# in through .png images, but we'll show you how to do that in
+# anither lab. For now, you'll see how to import .mats:
+mat = scipy.io.loadmat('D:\\learning\\DAT210x-master\\Module4\\Datasets\\face_data.mat')
+df = pd.DataFrame(mat['images']).T
+num_images, num_pixels = df.shape
+num_pixels = int(math.sqrt(num_pixels))
+
+# Rotate the pictures, so we don't have to crane our necks:
+for i in range(num_images):
+  df.loc[i,:] = df.loc[i,:].reshape(num_pixels, num_pixels).T.reshape(-1)
+
+
+#
+# TODO: Implement PCA here. Reduce the dataframe df down
+# to THREE components. Once you've done that, call Plot2D.
+#
+# The format is: Plot2D(T, title, x, y, num_to_plot=40):
+# T is your transformed data, NDArray.
+# title is your chart title
+# x is the principal component you want displayed on the x-axis, Can be 0 or 1
+# y is the principal component you want displayed on the y-axis, Can be 1 or 2
+#
+# .. your code here ..
+pca = PCA(n_components=3)
+pca.fit(df)
+PCA(copy=True, n_components=3, whiten=False)
+T = pca.transform(df)
+T
+Plot2D(T, title='pks', x=0,y=1, num_to_plot=40)
+
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#ax.set_xlabel('c1')
+#ax.set_ylabel('c2')
+#ax.set_zlabel('c3')
+
+
+#ax.scatter(T[0], T[1], T[2], c='r', marker='.')
+#plt.show()
+
+#
+# TODO: Implement Isomap here. Reduce the dataframe df down
+# to THREE components. Once you've done that, call Plot2D using
+# the first two components.
+#
+# .. your code here ..
+iso = manifold.Isomap(n_neighbors=3, n_components=3)
+iso.fit(df)
+manifold.Isomap(eigen_solver='auto', max_iter=None, n_components=2, n_neighbors=4,
+    neighbors_algorithm='auto', path_method='auto', tol=0)
+T = iso.transform(df)
+Plot2D(T, title='pks', x=0,y=1, num_to_plot=40)
+#
+# TODO: If you're up for a challenge, draw your dataframes in 3D
+# Even if you're not, just do it anyway.
+#
+# .. your code here ..
+
+
+plt.show()
---
+++ assignment5.py
@@ -0,0 +1,112 @@
+import pandas as pd
+from pathlib import Path
+from scipy import misc
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+import matplotlib.pyplot as plt
+import glob
+from sklearn import manifold
+
+# Look pretty...
+
+#img = misc.imread('D:\\learning\\DAT210x-master\\Module4\\Datasets\\ALOI\\32\\32_r0.png')
+
+matplotlib.style.use('ggplot')
+
+spl=glob.glob("D:\\learning\\DAT210x-master\\Module4\\Datasets\\ALOI\\32\*.png")
+
+
+#
+# TODO: Start by creating a regular old, plain, "vanilla"
+# python list. You can call it 'samples'.
+#
+# .. your code here .. 
+samples=[]
+
+# Open a file
+#import os, sys
+
+#path = "D:\\learning\\DAT210x-master\\Module4\\Datasets\\ALOI\\32"
+#dirs = os.listdir( path )
+#dirs
+
+#
+# TODO: Write a for-loop that iterates over the images in the
+# Module4/Datasets/ALOI/32/ folder, appending each of them to
+# your list. Each .PNG image should first be loaded into a
+# temporary NDArray, just as shown in the Feature
+# Representation reading.
+#
+# Optional: Resample the image down by a factor of two if you
+# have a slower computer. You can also convert the image from
+# 0-255  to  0.0-1.0  if you'd like, but that will have no
+# effect on the algorithm's results.
+#
+# .. your code here .. 
+for i in range(len(spl)):
+    img=misc.imread(spl[i])
+    img = img[::2, ::2]
+    X = (img / 255.0).reshape(-1)
+    samples.append(X)
+samples
+
+#path = "D:\learning\DAT210x-master\Module4\Datasets\ALOI\32"
+#glob.glob('D:\learning\DAT210x-master\Module4\Datasets\ALOI\32\*.xlsx')
+
+
+#
+# TODO: Once you're done answering the first three questions,
+# right before you converted your list to a dataframe, add in
+# additional code which also appends to your list the images
+# in the Module4/Datasets/ALOI/32_i directory. Re-run your
+# assignment and answer the final question below.
+#
+# .. your code here .. 
+
+
+#
+# TODO: Convert the list to a dataframe
+#
+# .. your code here .. 
+df = pd.DataFrame(samples)
+df
+
+
+#
+# TODO: Implement Isomap here. Reduce the dataframe df down
+# to three components, using K=6 for your neighborhood size
+#
+# .. your code here .. 
+iso = manifold.Isomap(n_neighbors=2, n_components=3)
+iso.fit(df)
+manifold.Isomap(eigen_solver='auto', max_iter=None, n_components=2, n_neighbors=2,
+    neighbors_algorithm='auto', path_method='auto', tol=0)
+T = iso.transform(df)
+ab=pd.DataFrame(T)
+
+
+
+#
+# TODO: Create a 2D Scatter plot to graph your manifold. You
+# can use either 'o' or '.' as your marker. Graph the first two
+# isomap components
+#
+# .. your code here .. 
+
+ab.plot.scatter(x=0, y=1)
+
+
+#
+# TODO: Create a 3D Scatter plot to graph your manifold. You
+# can use either 'o' or '.' as your marker:
+#
+# .. your code here .. 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('Final Grade')
+ax.set_ylabel('First Grade')
+ax.set_zlabel('Daily Alcohol')
+
+ax.scatter(ab[0], ab[1],ab[2], c='r', marker='.')
+plt.show()
+
